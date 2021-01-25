8474dc9bf103cb63d8ed316b05667bcedf782356
jessbakk <jessica.bakkers@gmail.com>
2021-01-24 17:37:49 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Blink twice if this works'));
+app.get('/', (req, res) => res.send('If you see this then the AWS Pipeline test was successful'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
